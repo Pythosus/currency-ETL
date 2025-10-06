5a355b2610e20e9dcbd2b221a0b27f197776d564
Pythosus <gg_aa_33@mail.ru>
2025-10-06 18:50:22 +0300

    bd fix + parser

--- base_init.sql
+++ base_init.sql
@@ -5,13 +5,13 @@ CREATE TABLE currency (
 	cur_name VARCHAR(37),
 	rub_cost DECIMAL(3, 4),
 	rec_date DATE DEFAULT NOW(),
-	PRIMARY KEY (num_code)
+	PRIMARY KEY (num_code, rec_date)
 );
 
 CREATE TABLE crypto (
-	word_code VARCHAR(3),
-	cru_name VARCHAR(25),
-	dol_cost DECIMAL(6,2),
+	word_code VARCHAR(10),
+	cru_name VARCHAR(50),
+	dol_cost DECIMAL(6,8),
 	rec_date DATE DEFAULT NOW(),
-	PRIMARY KEY (word_code)
+	PRIMARY KEY (word_code, rec_date)
 );
--- parser.py
+++ parser.py
@@ -1,17 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
-def currency_parser(url):
+#парсер валют
+def currency_parser(url) -> list:
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
     }
     data = []
+    #запрос
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
 
     sp = soup.find_all('tr')
-
+    #обработка
     for x in sp:
         dt = x.find_all('td')
         for i in range(len(dt)):
@@ -25,5 +28,26 @@ def currency_parser(url):
     return data[1:]
 
 
-def crypto_parser(url):
-    pass
+#парсер криптовалют
+def crypto_parser(url) -> list:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    }
+    #запрос
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    sp_name = soup.find_all('div',
+                             class_='tw-text-gray-700 dark:tw-text-moon-100 tw-font-semibold tw-text-sm tw-leading-5')
+    sp_cost = soup.find_all('td', class_='tw-text-end')
+    # обработка
+    data_cost = [x.get_text() for x in sp_cost]
+    data_cost = [float(re.sub(r'[^\d,.]', '', data_cost[i]).replace(',', '.'))
+                 for i in range(len(data_cost)) if i % 10 == 0]
+    data_name = [[y.strip(' ') for y in x.get_text().split('\n') if y.strip(' ') != ''] for x in sp_name][1:-8]
+    #print(max([len(x[-1]) for x in data_name]), max([len(x[0]) for x in data_name])) <- определение параметров для БД
+    data = [[data_name[i][1], data_name[i][0], data_cost[i]] for i in range(len(data_cost))]
+
+    return data
+
+#print(crypto_parser('https://www.coingecko.com/ru'))
+#print(currency_parser('https://cbr.ru/currency_base/daily'))
